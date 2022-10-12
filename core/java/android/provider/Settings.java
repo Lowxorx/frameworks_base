3d85a05386352187a55fbc6ba5e95e21476d5901
Pranav Vashi <neobuddy89@gmail.com>
2022-10-12 18:58:57 +0530

    Add interface for battery stats reset
    * Required for smart charging
    * Rewrite for A13
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6911,6 +6911,11 @@ public final class Settings {
         /**
          * @hide
          */
+        public static final String SMART_CHARGING_RESET_STATS = "smart_charging_reset_stats";
+
+        /**
+         * @hide
+         */
         public static final String SMART_CUTOFF = "smart_cutoff";
 
         /**
