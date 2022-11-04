96d30b96bd84d261de09f493b65dc74d52eaa008
jhenrique09 <jhsv09@gmail.com>
2022-11-04 15:40:04 -0300

    PixelPropsUtils: Get package name from context instead of app
    11-04 08:48:39.039 11637 11637 E AndroidRuntime: FATAL EXCEPTION: main
    11-04 08:48:39.039 11637 11637 E AndroidRuntime: Process: com.NextFloor.DestinyChild, PID: 11637
    11-04 08:48:39.039 11637 11637 E AndroidRuntime: java.lang.RuntimeException: Unable to get provider androidx.startup.InitializationProvider: androidx.startup.StartupException: android.content.pm.PackageManager$NameNotFoundException: ComponentInfo{/androidx.startup.InitializationProvider}
    11-04 08:48:39.039 11637 11637 E AndroidRuntime:   at android.app.ActivityThread.installProvider(ActivityThread.java:7488)
    
    Change-Id: Icb12f938fe0fca710f8f9d29182d0134ba3c63eb

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1246,7 +1246,7 @@ public class Instrumentation {
                 .instantiateApplication(cl, className);
         app.attach(context);
         AttestationHooks.initApplicationBeforeOnCreate(app);
-        String packageName = app.getPackageName();
+        String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
         return app;
     }
@@ -1266,7 +1266,7 @@ public class Instrumentation {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
         AttestationHooks.initApplicationBeforeOnCreate(app);
-        String packageName = app.getPackageName();
+        String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
         return app;
     }
